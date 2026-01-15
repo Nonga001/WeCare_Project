5770b237f8885f375d029952c978933a5323d777
Nonga001 <nongashaldon@gmail.com>
2026-01-15 11:02:59 +0300

    disable source maps in file vite.config.js

--- wecare-frontend/vite.config.js
+++ wecare-frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false, // Disable sourcemaps in production to avoid CSP eval issues
+  }
 })
